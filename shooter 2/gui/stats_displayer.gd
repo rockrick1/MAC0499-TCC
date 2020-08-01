742713b14a8a8519fec9cb482790af4d845fe8f5
Henrique Cerquinho <henriquecerquinho@gmail.com>
2020-08-01 13:17:46 -0300

    add fps counter

--- shooter 2/gui/stats_displayer.gd
+++ shooter 2/gui/stats_displayer.gd
@@ -3,3 +3,6 @@ extends VBoxContainer
 
 func update_stats(n_bullets):
 	$a.set_text("bullets on screen: "+str(n_bullets))
+
+func _process(delta):
+	$b.set_text("fps: "+str(Engine.get_frames_per_second()))
