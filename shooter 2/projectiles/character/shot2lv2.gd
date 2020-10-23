b2e14c927b730aa66aff0b2a09c7506799b87340
Henrique Cerquinho <henriquecerquinho@gmail.com>
2020-10-22 21:45:56 -0300

    remove prints and such

--- shooter 2/projectiles/character/shot2lv2.gd
+++ shooter 2/projectiles/character/shot2lv2.gd
@@ -5,8 +5,6 @@ func _ready():
 	$Projectile2.position.y += 5
 	$Projectile3.position.y += 5
 	for tween in $Tweens.get_children():
-		if tween:
-			print(tween.name)
 		var offset = -12
 		var proj = $Projectile2
 		if "2" in tween.get_name():
@@ -18,8 +16,3 @@ func _ready():
 		.1,
 		Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 		tween.start()
-
-
-func _on_1_tween_completed(object, key):
-	print("cabei!!!")
-	pass # Replace with function body.
