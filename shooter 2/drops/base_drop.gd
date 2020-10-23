92025a7a4947ba1b333a39a5dec292a8ada19bed
Henrique Cerquinho <henriquecerquinho@gmail.com>
2020-10-22 22:06:46 -0300

    remove obsolete method

--- shooter 2/drops/base_drop.gd
+++ shooter 2/drops/base_drop.gd
@@ -23,11 +23,6 @@ func die():
 	queue_free()
 
 
-func _on_DeathTimer_timeout():
-	character.gain_drop()
-	die()
-
-
 func _on_Drop_body_shape_entered(body_id, body, body_shape, area_shape):
 	# Collision with the sage border's bottom kills drop
 	if area_shape == 1 and body.get_name() == "StageBorder" and body_shape == 3:
